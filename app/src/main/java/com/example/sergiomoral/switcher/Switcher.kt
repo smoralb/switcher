5fec6aa6716b90907375af26a17dc9125f3edc95
sergio moral <smoralber@gmail.com>
2019-01-25 18:04:49 +0100

    Initial commit

---
+++ app/src/main/java/com/example/sergiomoral/switcher/Switcher.kt
@@ -0,0 +1,12 @@
+package com.example.sergiomoral.switcher
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Switcher : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_switcher)
+    }
+}
